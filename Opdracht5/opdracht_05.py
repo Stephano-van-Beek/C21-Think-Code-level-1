d082dde11a43e36087771ab9dfb17ca37cb6ad09
Stephano-van-beek <stephano.van.beek@gmail.com>
2021-10-28 10:01:03 +0200

    Favorite games gemaakt

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,3 +1,9 @@
 #maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
 #elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
 #weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
+def favorite_games (name):
+    print ("One of my favorite games is", name)
+
+favorite_games("Warframe")
+favorite_games("Minecraft")
+favorite_games("Genshin")
