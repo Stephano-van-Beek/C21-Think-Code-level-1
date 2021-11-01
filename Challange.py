75c03d5e19833d49a327df5e98df87942b6fe6ea
Stephano-van-beek <stephano.van.beek@gmail.com>
2021-11-01 10:35:31 +0100

    # toegevoegt

--- Challange.py
+++ Challange.py
@@ -1,9 +1,22 @@
-#Iets dat ik ga maken.
+#Challange.
+print("Hello en welkom met mijn lijst wat ik leuk vind")
 
+#intoductie
+name = "Stéphano"
+print ("Hello ik ben",name, "en ik ga jouw een lijst geven van mijn toppers") 
 
+#top 3 games die ik speel
 def favorite_games (name):
     print ("One of my favorite games is", name)
 
 favorite_games("Warframe")
 favorite_games("Minecraft")
 favorite_games("Genshin")
+
+#Top 3 movies
+def favorite_movies (name):
+    print ("One of my favorite movies is", name)
+
+favorite_movies("Joker")
+favorite_movies("End game")
+favorite_movies("Pirates of the caribbean")
