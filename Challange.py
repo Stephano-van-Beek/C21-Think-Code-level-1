8dd8ff22e78c404e425d9dfd87c841a2552d9b5d
Stephano-van-beek <stephano.van.beek@gmail.com>
2021-11-01 08:51:59 +0100

    challange aangemaakt

---
+++ Challange.py
@@ -0,0 +1,9 @@
+#Iets dat ik ga maken.
+
+
+def favorite_games (name):
+    print ("One of my favorite games is", name)
+
+favorite_games("Warframe")
+favorite_games("Minecraft")
+favorite_games("Genshin")
